7f46ec326bb0f4a2cdc684d8335ca4553851119e
nileshhaldar98 <nileshhaldar98@gmail.com>
2022-08-27 07:37:11 +0530

    mid header added

---
+++ Scripts/insight.js
@@ -0,0 +1,204 @@
+
+let post_data = [
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/future.jpg",
+      "post_image_url": "https://blog.fathomhealth.com/top-10-revenue-cycle-trends-for-2020",
+      "post_type": "Industry news",
+      "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+      "post_headline": "Top 10 revenue cycle trends",
+      "post_headline_url": "https://blog.fathomhealth.com/top-10-revenue-cycle-trends-for-2020",
+      "post_description": "1. A move towards pricing transparency 🔍 There have been public calls for more pricing transparency in healthcare for decades, but limited public int..."
+     },
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/mountaintop-1.png",
+      "post_image_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+      "post_type": "Product news",
+      "post_type_url": "https://blog.fathomhealth.com/tag/product-news",
+      "post_headline": "Fathom sets new industry automation rate benchmark with 96% live coding automation for emergency departments",
+      "post_headline_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+      "post_description": "SAN FRANCISCO, CA — August 2, 2022 — Fathom, the leader in AI medical coding automation, announced it is delivering the industry’s highest automation ..."
+     },
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/lightbulb.png",
+      "post_image_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+      "post_type": "Insider",
+      "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+      "post_headline": "What should HIM directors prioritize to ensure medical coding quality?",
+      "post_headline_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+      "post_description": "As an HIM director, you’re likely looking for ways to reduce inefficiencies to boost your organization’s bottom line and ensure PHI security."
+     },
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/building-colors.png",
+      "post_image_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+      "post_type": "Industry news",
+      "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+      "post_headline": "The future of revenue cycle from 3 perspectives: Vendors, providers, and payors",
+      "post_headline_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+      "post_description": "Like seemingly every other facet of healthcare, the way we manage the revenue cycle is evolving."
+     },
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/four.png",
+      "post_image_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+      "post_type": "Insider",
+      "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+      "post_headline": "Four RCM metrics for the healthcare C-suite",
+      "post_headline_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+      "post_description": "Medical coding quality and accuracy have a massive impact on your organization’s bottom line and revenue cycle. Whether that means gaining quick, regu..."
+     },
+     {
+      "post_image": "https://blog.fathomhealth.com/hubfs/accuracy.jpg",
+      "post_image_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+      "post_type": "Industry news",
+      "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+      "post_headline": "The secret to increasing medical coding accuracy",
+      "post_headline_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+      "post_description": "With reports indicating denied claims rates are reaching new highs, many medical organizations are scrambling to get their numbers in check."
+     }
+    ];
+    let post_data2 = [
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/future.jpg",
+         "post_image_url": "https://blog.fathomhealth.com/top-10-revenue-cycle-trends-for-2020",
+         "post_type": "Industry news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+         "post_headline": "Top 10 revenue cycle trends",
+         "post_headline_url": "https://blog.fathomhealth.com/top-10-revenue-cycle-trends-for-2020",
+         "post_description": "1. A move towards pricing transparency 🔍 There have been public calls for more pricing transparency in healthcare for decades, but limited public int..."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/mountaintop-1.png",
+         "post_image_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+         "post_type": "Product news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/product-news",
+         "post_headline": "Fathom sets new industry automation rate benchmark with 96% live coding automation for emergency departments",
+         "post_headline_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+         "post_description": "SAN FRANCISCO, CA — August 2, 2022 — Fathom, the leader in AI medical coding automation, announced it is delivering the industry’s highest automation ..."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/lightbulb.png",
+         "post_image_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+         "post_type": "Insider",
+         "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+         "post_headline": "What should HIM directors prioritize to ensure medical coding quality?",
+         "post_headline_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+         "post_description": "As an HIM director, you’re likely looking for ways to reduce inefficiencies to boost your organization’s bottom line and ensure PHI security."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/building-colors.png",
+         "post_image_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+         "post_type": "Industry news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+         "post_headline": "The future of revenue cycle from 3 perspectives: Vendors, providers, and payors",
+         "post_headline_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+         "post_description": "Like seemingly every other facet of healthcare, the way we manage the revenue cycle is evolving."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/four.png",
+         "post_image_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+         "post_type": "Insider",
+         "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+         "post_headline": "Four RCM metrics for the healthcare C-suite",
+         "post_headline_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+         "post_description": "Medical coding quality and accuracy have a massive impact on your organization’s bottom line and revenue cycle. Whether that means gaining quick, regu..."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/accuracy.jpg",
+         "post_image_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+         "post_type": "Industry news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+         "post_headline": "The secret to increasing medical coding accuracy",
+         "post_headline_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+         "post_description": "With reports indicating denied claims rates are reaching new highs, many medical organizations are scrambling to get their numbers in check."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/mountaintop-1.png",
+         "post_image_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+         "post_type": "Product news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/product-news",
+         "post_headline": "Fathom sets new industry automation rate benchmark with 96% live coding automation for emergency departments",
+         "post_headline_url": "https://blog.fathomhealth.com/fathom-sets-highest-industry-automation-rate-with-96-live-coding-automation-for-emergency-departments",
+         "post_description": "SAN FRANCISCO, CA — August 2, 2022 — Fathom, the leader in AI medical coding automation, announced it is delivering the industry’s highest automation ..."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/lightbulb.png",
+         "post_image_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+         "post_type": "Insider",
+         "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+         "post_headline": "What should HIM directors prioritize to ensure medical coding quality?",
+         "post_headline_url": "https://blog.fathomhealth.com/what-should-him-directors-prioritize-to-ensure-medical-coding-quality",
+         "post_description": "As an HIM director, you’re likely looking for ways to reduce inefficiencies to boost your organization’s bottom line and ensure PHI security."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/building-colors.png",
+         "post_image_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+         "post_type": "Industry news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+         "post_headline": "The future of revenue cycle from 3 perspectives: Vendors, providers, and payors",
+         "post_headline_url": "https://blog.fathomhealth.com/the-future-of-revenue-cycle-from-3-perspectives-vendors-providers-and-payors",
+         "post_description": "Like seemingly every other facet of healthcare, the way we manage the revenue cycle is evolving."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/four.png",
+         "post_image_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+         "post_type": "Insider",
+         "post_type_url": "https://blog.fathomhealth.com/tag/insider",
+         "post_headline": "Four RCM metrics for the healthcare C-suite",
+         "post_headline_url": "https://blog.fathomhealth.com/four-rcm-metrics-for-the-healthcare-c-suite",
+         "post_description": "Medical coding quality and accuracy have a massive impact on your organization’s bottom line and revenue cycle. Whether that means gaining quick, regu..."
+        },
+        {
+         "post_image": "https://blog.fathomhealth.com/hubfs/accuracy.jpg",
+         "post_image_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+         "post_type": "Industry news",
+         "post_type_url": "https://blog.fathomhealth.com/tag/industry-news",
+         "post_headline": "The secret to increasing medical coding accuracy",
+         "post_headline_url": "https://blog.fathomhealth.com/the-secret-to-increasing-medical-coding-accuracy",
+         "post_description": "With reports indicating denied claims rates are reaching new highs, many medical organizations are scrambling to get their numbers in check."
+        }
+       ];
+
+displayPost(post_data);
+ function displayPost(data){
+     let post = document.querySelector(`.post-container`);
+     data.forEach(element => {
+         let {post_image,post_type,post_headline,post_description}=element; 
+         let div = document.createElement(`div`);
+         div.setAttribute(`class`,`post-cards`);
+         let image = document.createElement(`img`);
+         image.src = post_image;
+         let div1 = document.createElement(`div`);
+         let post_type1 = document.createElement(`h4`);
+         post_type1.innerText = post_type;
+         let head = document.createElement(`h2`);
+         head.innerText = post_headline;
+         let post_desc = document.createElement(`p`);
+         post_desc.innerText = post_description; 
+         div1.append(post_type1,head,post_desc)
+         div.append(image,div1);
+         post.append(div)
+         
+});
+}
+
+function buttonOnOff(value){
+    if(value==true){
+        buttonDisplay();
+    }else{
+        document.getElementById(`bottom-button-1`).innerHTML=null;
+        displayPost(post_data2);
+    }
+
+}
+buttonOnOff(true);
+document.getElementById(`bottom-button`).addEventListener(`click`,function(){
+  let value = false;
+    buttonOnOff(value);
+    console.log(45345)
+});
+function buttonDisplay(){
+let button = document.getElementById(`bottom-button-1`);
+let button1 = document.createElement(`button`);
+button1.setAttribute(`id`,`bottom-button`)
+button1.innerText=`LOAD MORE POSTS`;
+button.append(button1);
+}
